d1102a8f90508bf557b065f852a764f5f3fd32ac
karan <karangupta199920@gmail.com>
2018-11-22 18:45:45 +0530

    added gameActive and corrected resultTextView

--- app/src/main/java/com/karan/braintrainer/MainActivity.java
+++ app/src/main/java/com/karan/braintrainer/MainActivity.java
@@ -12,7 +12,7 @@ import java.util.ArrayList;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
-
+    boolean gameActive =true;
     Button startButton;
     TextView resultTextView;
     TextView scoreTextView;
@@ -29,42 +29,43 @@ public class MainActivity extends AppCompatActivity {
     Button button4;
     Button playAgainButton;
     public void chooseAnswer(View view) {
+        if (gameActive) {
 
-        if (view.getTag().toString().equals(Integer.toString(locationOfCorrectAnswer))) {
-            score++;
-            resultTextView.setText("Correct!");
-        } else {
-            resultTextView.setText("Wronng!");
+            if (view.getTag().toString().equals(Integer.toString(locationOfCorrectAnswer))) {
+                score++;
+                resultTextView.setText("Correct!");
+            } else {
+                resultTextView.setText("Wrong!");
+            }
+            numberOfQuestions++;
+            scoreTextView.setText(Integer.toString(score) + "/" + Integer.toString(numberOfQuestions));
+            generateQuestion();
         }
-        numberOfQuestions++;
-        scoreTextView.setText(Integer.toString(score) + "/" + Integer.toString(numberOfQuestions));
-        generateQuestion();
     }
-
     public void playAgain(View view){
         score=0;
         numberOfQuestions=0;
+        gameActive=true;
         timerTextView.setText("10s");
         scoreTextView.setText("0/0");
         resultTextView.setText("");
         playAgainButton.setVisibility(View.INVISIBLE);
         generateQuestion();
-        new CountDownTimer(10000,1000){
+       new CountDownTimer(10100,1000){
 
             @Override
             public void onTick(long millisUntilFinished) {
-                timerTextView.setText(String.valueOf(millisUntilFinished/1000) + "s");
+                timerTextView.setText(String.valueOf((int)millisUntilFinished/1000) + "s");
             }
 
             @Override
             public void onFinish() {
                 playAgainButton.setVisibility(View.VISIBLE);
                 timerTextView.setText("0s");
-                resultTextView.setText("Your Score :" + Integer.toString(score)+ "/" + Integer.toString(numberOfQuestions));
+                resultTextView.setText("Your Score : " + Integer.toString(score)+ "/" + Integer.toString(numberOfQuestions));
+                gameActive=false;
             }
         }.start();
-
-
     }
 
     public void generateQuestion() {
@@ -99,7 +100,7 @@ public class MainActivity extends AppCompatActivity {
 
     startButton.setVisibility(View.INVISIBLE);
     layout.setVisibility(RelativeLayout.VISIBLE);
-    playAgain(findViewById(R.id.playAgainButton));
+       playAgain(findViewById(R.id.playAgainButton));
 }
 
     @Override
@@ -113,9 +114,10 @@ public class MainActivity extends AppCompatActivity {
         button2 = (Button) findViewById(R.id.button2);
         button3 = (Button) findViewById(R.id.button3);
         button4 = (Button) findViewById(R.id.button4);
-        resultTextView = (TextView) findViewById(R.id.resultTextVIew);
+        resultTextView = (TextView) findViewById(R.id.resultTextView);
         scoreTextView = (TextView) findViewById(R.id.scoreTextView);
         playAgainButton = (Button) findViewById(R.id.playAgainButton);
         layout=(RelativeLayout) findViewById(R.id.relativeLayout);
+        timerTextView=(TextView)findViewById(R.id.timerTextView);
     }
 }
